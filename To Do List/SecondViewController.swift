15b6e4f53cbe68baacd682e271b678cc64cf3180
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-09-19 19:05:22 -0400

    Initial Commit

---
+++ To Do List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Nathanial L. McConnell on 9/19/14.
+//  Copyright (c) 2014 Nathanial L. McConnell. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
